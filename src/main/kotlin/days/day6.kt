0574c4a8d43f90dd0cd6791d8930c5b78611572f
Hugo van der Zwaag <hugo.vanderzwaag@ns.nl>
2022-12-06 10:56:24 +0100

    Even shorter

--- src/main/kotlin/days/day6.kt
+++ src/main/kotlin/days/day6.kt
@@ -4,15 +4,11 @@ import Exercise
 
 class Day6 : Exercise {
     override fun a(input: List<String>): String {
-        val inputLine = input.first()
-        val windowSize = 4
-        return findFirstWindow(inputLine, windowSize)
+        return findFirstWindow(input.first(), 4)
     }
 
     override fun b(input: List<String>): String {
-        val inputLine = input.first()
-        val windowSize = 14
-        return findFirstWindow(inputLine, windowSize)
+        return findFirstWindow(input.first(), 14)
     }
 
     private fun findFirstWindow(inputLine: String, windowSize: Int) = inputLine.toList()
